426999b91230275cffbca879bf9e7dea1fbbe1ce
Garrad Belle <garrad.d.belle@gmail.com>
2021-10-25 11:21:32 -0400

    First Commit

---
+++ src/main/java/mod3/Cap3Application.java
@@ -0,0 +1,13 @@
+package mod3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cap3Application.class, args);
+	}
+
+}
